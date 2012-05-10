1e2d02b461855a1d4a94da447e4f76b8c432203f
joyossarian <joyossarian@gmail.com>
2012-05-10 17:31:52 +0800

    updated May.10th, 2012

--- Cyclone.NET/Core.cs
+++ Cyclone.NET/Core.cs
@@ -7,5 +7,20 @@ namespace Cyclone.NET
 {
     public class Vector3
     {
+        public double x;
+        public double y;
+        public double z;
+
+        public Vector3()
+        {
+            x = y = z = 0.0f;
+        }
+
+        public Vector3(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
     }
 }
