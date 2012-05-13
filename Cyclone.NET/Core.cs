0ed0eb63bbae4607bc778e9d904c03da77156cd3
jotiger <joyossarian@gmail.com>
2012-05-14 01:31:54 +0800

    vector3 implementation and simple ut

--- Cyclone.NET/Core.cs
+++ Cyclone.NET/Core.cs
@@ -40,8 +40,53 @@ namespace Cyclone.NET
         static readonly Vector3 Y = new Vector3(0, 1, 0);
         static readonly Vector3 Z = new Vector3(0, 0, 1);
 
-        double operator[](UInt16 i) const
+        public double this[UInt16 index]
         {
-    }
+            get {
+                switch (index)
+                {
+                    case 0:
+                        return x;
+                    case 1:
+                        return y;
+                    case 2:
+                        return z;
+                    default:
+                        throw new Exception(string.Format("{0} wrong Vector3 index!", index));
+                }
+            }
+            set {
+                switch (index)
+                {
+                    case 0:
+                        {
+                            x = value;
+                        }
+                        break;
+                    case 1:
+                        {
+                            y = value;
+                        }
+                        break;
+                    case 2:
+                        {
+                            z = value;
+                        }
+                        break;
+                    default:
+                        throw new Exception(string.Format("{0} wrong Vector3 index!", index));
+                }
+            }
+        }
+
+        public static Vector3 operator  + (Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
     }
 }
---
+++ UTCyclone.NET/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Cyclone.NET;
+
+namespace UTCyclone.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3 t1 = new Vector3(1.0f, 1.0f, 1.0f);
+            Vector3 t2 = new Vector3(2.0f, 2.0f, 2.0f);
+            Vector3 t3 = t1 + t2;
+            t1 += t3;
+            Console.WriteLine("x {0}, y {1}, z{1}", t3.x, t3.y, t3.z);
+            Console.WriteLine("x {0}, y {1}, z{1}", t1.x, t1.y, t1.z);
+            t3 -= t1;
+            Console.WriteLine("x {0}, y {1}, z{1}", t3.x, t3.y, t3.z);
+        }
+    }
+}
