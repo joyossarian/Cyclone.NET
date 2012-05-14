0d113b2ee906be6d0d9657b07838f84f0e1cda6a
jotiger <joyossarian@gmail.com>
2012-05-14 20:43:41 +0800

    add * support

--- Cyclone.NET/Core.cs
+++ Cyclone.NET/Core.cs
@@ -84,9 +84,14 @@ namespace Cyclone.NET
             return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
-        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        public static Vector3 operator - (Vector3 v1, Vector3 v2)
         {
             return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        public static Vector3 operator * (Vector3 v, double value)
+        {
+            return new Vector3(v.x * value, v.y * value, v.z * value);
         }
     }
 }
